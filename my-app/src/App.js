f2e7e8aa83978adf344a0552a98d834d542bac91
Getaneh Biratu <mrgetaneh@gmail.com>
2024-10-28 19:17:38 +0000

    add class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,18 @@
 import css from './App.module.css';
+import Sidebar from './components/Sidebar';
+import NavBarSimple from './components/NavBarSimple';
+
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      { /*Add ur comment*/} 
+      <Sidebar />
+       <b></b>
+
+      <NavBarSimple />
+      
+    
     </div>
   );
 }
---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import css from "./css/Sidebar.module.css";
+
+function Sidebar() {
+    return <div div className={css.sidebar}> <a href= "#"
+ target= "blank"> My Photos</a>
+ <a href= "#"
+ target= "blank"> My Illustrations</a>
+ <a href= "#"
+ target= "blank"> My Paintings</a>
+</div>
+}
+
+export default Sidebar;
